c4d891e3f934f7d1765feb8a40714be17195510f
jkennel <jkennel@uoguelph.ca>
2019-07-24 06:29:02 -0400

    add data

--- R/mrc.R
+++ R/mrc.R
@@ -1,7 +1,9 @@
+# This code is based on the code here
+# https://wwwrcamnl.wr.usgs.gov/uzf/EMR_Method/EMR.method.html
 
-check_precip <- function(x, precip_col = 'precip') {
+check_precip <- function(x, precip_var = 'precip') {
 
-  if (any(diff(x[[precip_col]]) < 0)) {
+  if (any(diff(x[[precip_var]]) < 0)) {
     stop('Precipitation must monotonically increase')
   }
 
@@ -42,18 +44,28 @@ get_dry <- function(x,
 #'
 #' @param x data
 #' @param time_var name of time col
+#' @param wl_var name of water level col
 #' @param n number of values in regression
 #'
 #' @return data.table with labelled 'dry' sections
 #'
 #' @export
 #'
-get_segments <- function(x, time_var = 'datetime', n = 48) {
+get_segments <- function(x,
+                         time_var = 'datetime',
+                         wl_var = 'wl',
+                         n = 48) {
 
-  x[is_dry == FALSE, val := NA_real_]
-  x[, slope := roll_lm(as.matrix(datetime), as.matrix(val),
+  # remove dry values
+  x[, val_use := get(wl_var)]
+  x[is_dry == FALSE, val_use := NA_real_]
+
+  # calculate slopes
+  x[, slope := roll_lm(as.matrix(datetime), as.matrix(val_use),
           width = n, intercept = TRUE, min_obs = n)$coefficients[,2]]
-  x[, mean_val := roll_mean(as.matrix(val),
+
+
+  x[, mean_val := roll_mean(as.matrix(val_use),
                          width = n, min_obs = n)]
   x
 
@@ -61,33 +73,28 @@ get_segments <- function(x, time_var = 'datetime', n = 48) {
 
 #' mrc
 #'
-#' @inheritParams recipes::step_lag
-#' @inheritParams harmonic
+#' @inheritParams get_segments
+#' @inheritParams get_dry
 #'
 #' @return data.table with mrc
 #' @export
 #'
+#' @example
+#' data(mrc_dat)
+#' mrc(x=mrc_dat)
 mrc <- function(x,
                 time_var = 'datetime',
                 precip_var = 'precip',
+                wl_var = 'wl',
                 n = 48,
                 lag = 48,
                 cutoff = 0.01) {
 
+  check_precip(x, precip_var)
+
   out <- get_dry(x, precip_var, lag, cutoff)
-  out <- get_segments(out, time_var, n)
+  out <- get_segments(out, time_var, wl_var, n)
   out
 }
 
-# library(data.table)
-# library(roll)
-# library(recharge)
-# library(usethis)
-# fn <- "/media/kennel/Data/tmp/EMR_input.(mNW95sampledata).csv"
-# z  <- fread(fn)
-# setnames(z, c('datetime', 'val', 'precip'))
-#
-# tmp <- mrc(x=z)
-# plot(mean_val~slope, tmp[slope < 0.2],
-#      pch = 20)
 
